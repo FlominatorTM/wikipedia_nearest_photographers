0684a63bf39b3f681ef4e2e325da3b763108dc85
FlominatorTM <flominator@gmx.net>
2015-06-13 21:31:47 +0200

    adding support for it.wp

--- next_inc/proj/it.wikipedia.org.php
+++ next_inc/proj/it.wikipedia.org.php
@@ -3,6 +3,6 @@
 $config['UserPrefixMale'] = 'utente';
 $config['UserPrefixFemale'] = 'utente'; //?
 $config['IndexUserColumn'] = 0; //zero indexed
-$config['IndexLocationColumn'] = 3; //zero indexed
+$config['IndexLocationColumn'] = 2; //zero indexed
 
 $createdPage = new OfferPageTable("it.wikipedia.org", 'Wikipedia:Wikipediani/Fotografi', $config);
