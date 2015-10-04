6d1c09e15989d4e22dbb44e75138da5894ca02fa
FlominatorTM <flominator@gmx.net>
2015-10-04 10:06:10 +0200

    of => von

--- next_inc/lang/de.php
+++ next_inc/lang/de.php
@@ -24,7 +24,7 @@ $messages['template_location'] = 'Standort';
 $messages['template_range'] = 'Aktionsradius';
 
 //result
-$messages['cache_outdated'] = "Herunterladen einer neuen Version of __PAGE__ und deren Einträgen, da die gespeicherte Version veraltet war. Dies kann einige Minuten dauern. Bitte sei geduldig.";
+$messages['cache_outdated'] = "Herunterladen einer neuen Version von __PAGE__ und deren Einträgen, da die gespeicherte Version veraltet war. Dies kann einige Minuten dauern. Bitte sei geduldig.";
 $messages['user_retrieval'] = "Benutzer __USER__ wird ausgelesen";
 $messages['distance_to'] = 'Fotografen nach Entfernung zu Koordinaten aus [[_ARTICLE_TO_]] ';
 $messages['no_coordinates'] = 'Der Artikel _LOCATION_ ist ungültig oder hat keine Koordinaten.';
