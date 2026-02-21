9642c405c1be68f07c52330577ddb605ebddab59
Flominatortm <flominator@gmx.net>
2026-02-21 03:15:47 +0100

    only formatting

--- foto_range.php
+++ foto_range.php
@@ -3,120 +3,101 @@
 require_once("setup.php");
 
 $article_to  = "";
-if(isset($_REQUEST['article_to']))
-{
+if (isset($_REQUEST['article_to'])) {
 	$article_to = $_REQUEST['article_to'];
 }
 
 $radius = 0;
-if(isset($_REQUEST['radius']))
-{
-	$radius = $_REQUEST['radius']+0;
+if (isset($_REQUEST['radius'])) {
+	$radius = $_REQUEST['radius'] + 0;
 }
 
-if(!isset($_REQUEST['go']))
-{
-	echo '<h1>' . $I18N->msg( 'headline' ) . '</h1>';
+if (!isset($_REQUEST['go'])) {
+	echo '<h1>' . $I18N->msg('headline') . '</h1>';
 	echo '<form>';
-	echo $I18N->msg( 'lang') . ': <input name="lang" value="' . $lang .'"/> ' . $I18N->msg( 'lang_example') .'<br>';
-	echo $I18N->msg( 'project') . ': <input name="project" value="' . $project .'"/>' . $I18N->msg( 'project_example') .'<br>';
-	echo $I18N->msg( 'article_to') . ': <input name="article_to" value="' . $article_to .'"/>' . $I18N->msg( 'article_to_descr') .'<br>';
-	
-	echo $I18N->msg( 'radius') . ': <input name="radius" value="' . $radius .'"/>' . $I18N->msg( 'radius_descr') .'<br>';
-	echo '<input type="submit" name="go" value="'. $I18N->msg( 'find_next') .'"/>';
+	echo $I18N->msg('lang') . ': <input name="lang" value="' . $lang . '"/> ' . $I18N->msg('lang_example') . '<br>';
+	echo $I18N->msg('project') . ': <input name="project" value="' . $project . '"/>' . $I18N->msg('project_example') . '<br>';
+	echo $I18N->msg('article_to') . ': <input name="article_to" value="' . $article_to . '"/>' . $I18N->msg('article_to_descr') . '<br>';
+
+	echo $I18N->msg('radius') . ': <input name="radius" value="' . $radius . '"/>' . $I18N->msg('radius_descr') . '<br>';
+	echo '<input type="submit" name="go" value="' . $I18N->msg('find_next') . '"/>';
 	echo '</form>';
-}
-else
-{
+} else {
 	log_search();
 	$locTo;
 	$headLine;
 
-	if($article_to != "")
-	{
-		$linkToArticleTo = "<a href=\"https://$server/wiki/".name_in_url($article_to)."\">$article_to</a>";
+	if ($article_to != "") {
+		$linkToArticleTo = "<a href=\"https://$server/wiki/" . name_in_url($article_to) . "\">$article_to</a>";
 		$locTo = GeoLocation::FromArticle($article_to, $server);
-		$headLine = $I18N->msg( 'distance_to', array('variables' =>array($linkToArticleTo)));
-	}
-	else if($_REQUEST['lat'] && $_REQUEST['lon'])
-	{
-		$lat = $_REQUEST['lat']+0;
-		$lon = $_REQUEST['lon']+0;
-		$name = $lat.'/'.$lon;
+		$headLine = $I18N->msg('distance_to', array('variables' => array($linkToArticleTo)));
+	} else if ($_REQUEST['lat'] && $_REQUEST['lon']) {
+		$lat = $_REQUEST['lat'] + 0;
+		$lon = $_REQUEST['lon'] + 0;
+		$name = $lat . '/' . $lon;
 		$locTo = GeoLocation::Direct($lat, $lon, $name);
-		$headLine = $I18N->msg( 'distance_to_coord', array('variables' =>array($name)));
-	}
-	else
-	{
-		die ("error: missing parameters");
+		$headLine = $I18N->msg('distance_to_coord', array('variables' => array($name)));
+	} else {
+		die("error: missing parameters");
 	}
 
-	echo '<h1>' . $headLine .'</h1>';	
-    
-    $footNote = "";
-    if($locTo->IsValid())
-    {
+	echo '<h1>' . $headLine . '</h1>';
+
+	$footNote = "";
+	if ($locTo->IsValid()) {
 		$allOfferPages = new OfferPages($server);
 		$allOfferPages->MergeOffers($locTo);
 		$allOfferPages->ListUsersToRequest($radius);
 		$homePage = $allOfferPages->HomeOfferPage;
-		$footNote = str_replace('_OFFER_PAGE_', $homePage->Link, $I18N->msg( 'you_on_list'));
-		$footNote = str_replace($homePage->server.'<', $homePage->OfferPageName.'<', $footNote);
-    }
-    else
-    {
-	echo $I18N->msg( 'no_coordinates', array('variables' => array($linkToArticleTo)) );
-    }
+		$footNote = str_replace('_OFFER_PAGE_', $homePage->Link, $I18N->msg('you_on_list'));
+		$footNote = str_replace($homePage->server . '<', $homePage->OfferPageName . '<', $footNote);
+	} else {
+		echo $I18N->msg('no_coordinates', array('variables' => array($linkToArticleTo)));
+	}
 
-    echo "<br><br><a href=\"?lang=$lang&project=$project&radius=$radius\">".$I18N->msg( 'new_request')."</a>";
-    echo "<br><hr>$footNote";
+	echo "<br><br><a href=\"?lang=$lang&project=$project&radius=$radius\">" . $I18N->msg('new_request') . "</a>";
+	echo "<br><hr>$footNote";
 }
 
-    echo "<br><hr>";
-    echo '<a href="' . $I18N->msg( 'manual_link') . '">'. $I18N->msg( 'manual')  . '</a>';
-    echo " - ";
-    echo '<a href="' . $I18N->msg( 'issue_link') . '">'. $I18N->msg( 'issues')  . '</a>';
-    echo " - ";
-    echo '<a href="foto_check.php">'. $I18N->msg( 'check_users')  . '</a>';
-    echo ' - by <a href="http://de.wikipedia.org/wiki/Benutzer:Flominator">Flominator</a>';
-	//echo $I18N->getFooterLine( TSINT_HELP_ALL ); 
+echo "<br><hr>";
+echo '<a href="' . $I18N->msg('manual_link') . '">' . $I18N->msg('manual')  . '</a>';
+echo " - ";
+echo '<a href="' . $I18N->msg('issue_link') . '">' . $I18N->msg('issues')  . '</a>';
+echo " - ";
+echo '<a href="foto_check.php">' . $I18N->msg('check_users')  . '</a>';
+echo ' - by <a href="http://de.wikipedia.org/wiki/Benutzer:Flominator">Flominator</a>';
+//echo $I18N->getFooterLine( TSINT_HELP_ALL ); 
 
-function log_search ()
+function log_search()
 {
 	global $article_to, $lang, $project, $server, $user_lang;
-	if(!is_dir("log")) mkdir ("log");
-	$logfile = "log/nearest_".strftime("%Y-%m-%d").".csv";
+	if (!is_dir("log")) mkdir("log");
+	$logfile = "log/nearest_" . strftime("%Y-%m-%d") . ".csv";
 
 	$header = "";
-	if(!file_exists($logfile))
-	{
-		$header="Day;";
-		$header.="Time;";
-		$header.="IP (Client);";
-		$header.="UI Language;";
-		$header.="Article;";
-		$header.="Language;";
-		$header.="Project;";
-		$header.="User-Agent;";
-		$header.="\n";
+	if (!file_exists($logfile)) {
+		$header = "Day;";
+		$header .= "Time;";
+		$header .= "IP (Client);";
+		$header .= "UI Language;";
+		$header .= "Article;";
+		$header .= "Language;";
+		$header .= "Project;";
+		$header .= "User-Agent;";
+		$header .= "\n";
 	}
-	
-	if($file = fopen($logfile, "a"))
-	{
+
+	if ($file = fopen($logfile, "a")) {
 		fputs($file, $header);
-		fputs($file, strftime("%Y-%m-%d").";");
-		fputs($file, strftime("%H:%M").";");
-		fputs($file, "\"".$_SERVER['REMOTE_ADDR']."\";");
-		fputs($file, $user_lang.";");
-		fputs($file, "\"".$article_to."\";");
-		fputs($file, $lang.";");
-		fputs($file, $project.";");
-		fputs($file, '"'.$_SERVER['HTTP_USER_AGENT'].'"'.";");
+		fputs($file, strftime("%Y-%m-%d") . ";");
+		fputs($file, strftime("%H:%M") . ";");
+		fputs($file, "\"" . $_SERVER['REMOTE_ADDR'] . "\";");
+		fputs($file, $user_lang . ";");
+		fputs($file, "\"" . $article_to . "\";");
+		fputs($file, $lang . ";");
+		fputs($file, $project . ";");
+		fputs($file, '"' . $_SERVER['HTTP_USER_AGENT'] . '"' . ";");
 		fputs($file, "\n");
 		fclose($file);
 	}
-	
 }
-
-
-?>
--- setup.php
+++ setup.php
@@ -1,28 +1,27 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$I18N = new Intuition( 'next_photographer' );
-$I18N->registerDomain( 'next_photographer', __DIR__ . '/next_inc/lang/' );
+$I18N = new Intuition('next_photographer');
+$I18N->registerDomain('next_photographer', __DIR__ . '/next_inc/lang/');
 
 forward_output_directly();
- 
+
 echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
 
 include("shared_inc/language.inc.php");
 include("shared_inc/wiki_functions.inc.php");
-include('next_inc/OfferingUser.php');	
-include('next_inc/OfferPage.php');	
-include('next_inc/OfferPageTemplate.php');	
-include('next_inc/OfferPageList.php');	
-include('next_inc/OfferPageTable.php');	
-include('next_inc/OfferPageTableFromFile.php');	
-include('next_inc/OfferPages.php');	
-include('next_inc/GeoLocation.php');	
+include('next_inc/OfferingUser.php');
+include('next_inc/OfferPage.php');
+include('next_inc/OfferPageTemplate.php');
+include('next_inc/OfferPageList.php');
+include('next_inc/OfferPageTable.php');
+include('next_inc/OfferPageTableFromFile.php');
+include('next_inc/OfferPages.php');
+include('next_inc/GeoLocation.php');
 
 $is_debug = false;
-if(isset($_REQUEST['debug']))
-{
-    $is_debug = ($_REQUEST['debug']=="on" || $_REQUEST['debug']=="true" );
+if (isset($_REQUEST['debug'])) {
+	$is_debug = ($_REQUEST['debug'] == "on" || $_REQUEST['debug'] == "true");
 }
 
 $server = "$lang.$project.org";
@@ -32,22 +31,19 @@ function forward_output_directly()
 {
 	//copied from http://www.andrew-kirkpatrick.com/2013/08/output-csv-straight-to-browser-using-php/
 	//prevent apache from buffering it for deflate/gzip
-	header('Content-Type: text/html; charset=utf-8'); 
+	header('Content-Type: text/html; charset=utf-8');
 	header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
 
 	// Turn off output buffering
 	ini_set('output_buffering', 'off');
 	// Turn off PHP output compression
 	ini_set('zlib.output_compression', false);
-			 
+
 	//Flush (send) the output buffer and turn off output buffering
 	//ob_end_flush();
 	while (@ob_end_flush());
-			 
+
 	// Implicitly flush the buffer(s)
 	ini_set('implicit_flush', true);
 	ob_implicit_flush(true);
 }
-	
-
-	
